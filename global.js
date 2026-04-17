98463c9d4d30e355211761731ae3650aee4ec2d5
Sahar Zahir <sazahir@ucsd.edu>
2026-04-16 22:14:35 -0700

    link

--- global.js
+++ global.js
@@ -17,9 +17,14 @@ let pages = [
 let nav = document.createElement("nav");
 document.body.prepend(nav);
 
+const BASE_PATH = 
+    (location.hostname == "localhost" || location.hostname == "127.0.0.1")
+
 for (let p of pages) {
     let url = p.url;
     let title = p.title;
     
+    url = !url.startsWith("http") ? BASE_PATH + url : url;
+
     nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`)
 }
