3cfccb6c665a4713772ca82a8fc788e05b863c2a
Sahar Zahir <sazahir@ucsd.edu>
2026-04-24 22:00:54 -0700

    Update projects.js

--- Projects/projects.js
+++ Projects/projects.js
@@ -5,3 +5,9 @@ const projects = await fetchJSON('../lib/projects.json');
 const projectsContainer = document.querySelector('.projects');
 
 renderProjects(projects, projectsContainer, 'h2');
+
+const title = document.querySelector('.projects-title');
+
+if (title) {
+  title.textContent = `${projects.length} Projects`;
+}
