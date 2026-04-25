e4fc5676cbb4e5dbea9cadb80eaf2425723b2f24
Sahar Zahir <sazahir@ucsd.edu>
2026-04-24 19:51:10 -0700

    projects

--- global.js
+++ global.js
@@ -99,3 +99,19 @@ export async function fetchJSON(url) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
+
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    containerElement.innerHTML = '';
+
+    for (let project of projects) {
+        const article = document.createElement('article');
+
+        article.innerHTML = `
+        <${headingLevel}>${project.title}</${headingLevel}>
+        <img src="${project.image}" alt="${project.title}">
+        <p>${project.description}</p>
+        `;
+        containerElement.appendChild(article);
+    
+    }
+}
