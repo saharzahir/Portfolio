8d6c7fe3d4004de0de547e922fbfe2fb2d5e39a7
Sahar Zahir <sazahir@ucsd.edu>
2026-05-01 17:26:18 -0700

    pie chart

--- Projects/projects.js
+++ Projects/projects.js
@@ -1,4 +1,5 @@
 import { fetchJSON, renderProjects } from '../global.js';
+import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 
 const projects = await fetchJSON('../lib/projects.json');
 
@@ -11,3 +12,15 @@ const title = document.querySelector('.projects-title');
 if (title) {
   title.textContent = `${projects.length} Projects`;
 }
+
+let arcGenerator = d3.arc().innerRadius(0).outerRadius(50);
+
+let arc = arcGenerator({
+  startAngle: 0,
+  endAngle: 2 * Math.PI,
+});
+
+d3.select('#projects-pie-plot')
+  .append('path')
+  .attr('d', arc)
+  .attr('fill', 'red');
